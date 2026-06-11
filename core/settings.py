a2768e272e96c6ae32ddc6d32734e437067425df
Prem <pypremyadav123@gmail.com>
2026-06-11 14:19:11 +0530

    Switch to dj-database-url for production

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url  # Ye line add karni thi
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,11 +55,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+# Updated Database Configuration
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+    )
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
